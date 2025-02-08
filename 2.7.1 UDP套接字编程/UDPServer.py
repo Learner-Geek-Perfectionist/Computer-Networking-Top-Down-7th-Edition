f185e25fe4a2a05f453fb5f25b24b1572ec80fbd
ouyangzhaoxin <1273730703@qq.com>
2025-02-08 15:03:55 +0800

    update

--- 2.7.1 UDP套接字编程/UDPServer.py
+++ 2.7.1 UDP套接字编程/UDPServer.py
@@ -4,6 +4,6 @@ serverSocket = socket(AF_INET, SOCK_DGRAM) # 同样地，使用 UDP 协议
 serverSocket.bind(('', serverPort)) # server 端的套接字和 12000 端口号绑定，
 print('The Server is ready to receive')
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
+    message, clientAddress = serverSocket.recvfrom(2048) # 服务端，通过「套接字」收报文
     modifiedMessage = message.decode().upper() # 将字符串转换为大写
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress) # 转换之后的字符串发送给客户端
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress) # 服务端，通过「套接字」发报文
